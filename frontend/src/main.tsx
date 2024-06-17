c62c764b338cb052b30914369a1ed92b256b58a9
Guilherme Thomas da Costa <guilhermethomas.dev@gmail.com>
2024-06-17 19:40:19 -0300

    first commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import HomePage from './pages/homePage'
+import ProfilesPage from './pages/profilesPage'
+import ErrorPage from './pages/errorPage'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <HomePage/>,
+  errorElement: <ErrorPage/>,
+},
+{
+  path:'/profiles',
+  element: <ProfilesPage/>
+}
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
---
+++ frontend/src/pages/profilesPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function ProfilesPage() {
+    const profiles = [1,2,3,4,5];
+    return(
+        <div>
+            {profiles.map((profile)=>
+                <Link key={profile} to={`/profiles/${profile}`}></Link>
+            )}
+        </div>
+    )
+}
